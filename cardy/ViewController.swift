db1fd1099d3b18879a7de1bef1c3218868f9a627
91.eahz@gmail.com <EHZ@ericks-macbook-pro.local>
2016-04-07 22:03:54 -0400

    second commit

--- cardy/ViewController.swift
+++ cardy/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("first commit")
+        print("second commit")
     }
 
     override func didReceiveMemoryWarning() {
