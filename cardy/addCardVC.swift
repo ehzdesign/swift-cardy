08417702f24dae97dc98aaba52bc553c3c4b04bd
91.eahz@gmail.com <EHZ@ericks-macbook-pro.local>
2016-04-22 00:30:52 -0400

    input field ui changed

--- cardy/addCardVC.swift
+++ cardy/addCardVC.swift
@@ -16,26 +16,99 @@ class addCardVC: UIViewController {
     //label for company name on card
     @IBOutlet weak var companyLabel: UILabel!
     
-    var newCard = []
+    //label for amount on card
+    @IBOutlet weak var amountLabel: UILabel!
    
+    //cardNumber Label
+    @IBOutlet weak var cardNumberLabel: UILabel!
+
+   
+    //company name field
+    @IBOutlet weak var companyNameField: UITextField!
+
+    //amountField
+    @IBOutlet weak var amountField: UITextField!
+//    card number field
+    @IBOutlet weak var cardNumberField: UITextField!
     
-    //link to firebase database
+    //this might link to firebase database were hoping
     var ref = Firebase(url: "https://vivid-torch-2205.firebaseio.com/")
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //watch company name input and adjust text on company label
-        ref.observeEventType(.Value, withBlock: {
-            snapshot in self.companyLabel.text = snapshot.value as? String
-        })
+        UILabel.appearanceWhenContainedInInstancesOfClasses([UITextField.self]).textColor = UIColor(hue: 0, saturation: 0, brightness: 0.9, alpha: 1.0) /* #e5e5e5 */
+
+        
+       self.companyNameField.backgroundColor = UIColor.clearColor()
+        self.companyNameField.layer.cornerRadius = 8.0
+        self.companyNameField.layer.masksToBounds = true
+        
+         self.companyNameField.layer.borderWidth = 1
+       self.companyNameField.layer.borderColor = UIColor.whiteColor().CGColor
+        
 
 
         
     }
 
+    //live change amount on card
+    @IBAction func amountLabelUpdate(sender: UITextField) {
+        
+        if (sender.text!.isEmpty){
+            amountLabel.text = "$0"
+            
+        }else{
+            amountLabel.text = "$" + (sender.text)!
+        }
+        
+    }
+    
+    //live change companyName on card
     @IBAction func companyNameLabel(sender: UITextField) {
-        ref.setValue(sender.text)
+        
+        if (sender.text!.isEmpty){
+            companyLabel.text = "company name"
+          
+        }else{
+           companyLabel.text = (sender.text)
+        }
+        
+    }
+    
+    
+    @IBAction func cardNumberUpdate(sender: UITextField) {
+        
+        if (sender.text!.isEmpty){
+            cardNumberLabel.text = "000000000000"
+           
+        }else{
+            cardNumberLabel.text = (sender.text)
+        }
+        
+    }
+    
+    
+    //this function will hopefully add content to new Card Dictionary
+    @IBAction func saveCardAction(sender: AnyObject) {
+     //get text from compnay name field and save to new Card dictionary
+        
+        let cardName = "cards/" + companyNameField.text! + "Card"
+        let newCardRef = ref.childByAppendingPath(String(cardName))
+        
+        
+        
+        
+        var newCard = [String: String]()
+    
+        
+        newCard["companyName"] = companyNameField.text
+        newCard["amount"] = amountField.text
+        newCard["cardNumber"] = cardNumberField.text
+        
+        newCardRef.updateChildValues(newCard)
+        
+     
     }
 }
