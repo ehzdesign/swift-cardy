d56be3557506a27ee5f80c04848e9e862d8e7c3c
91.eahz@gmail.com <EHZ@ericks-macbook-pro.local>
2016-04-22 02:50:34 -0400

    retrieving all card info and watching for new cards being added

--- cardy/discoverVC.swift
+++ cardy/discoverVC.swift
@@ -8,14 +8,23 @@
 
 
 import UIKit
+import Firebase
 
 class discoverVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let ref = Firebase(url:"https://vivid-torch-2205.firebaseio.com/usersCards")
+        // Attach a closure to read the data at our posts reference
+        ref.observeEventType(.ChildAdded, withBlock: { snapshot in
+            print(snapshot.value.objectForKey("amount"))
+            print(snapshot.value.objectForKey("cardNumber"))
+            print(snapshot.value.objectForKey("companyName"))
+            }, withCancelBlock: { error in
+                print(error.description)
+        })
         
-        self.view.backgroundColor = UIColor.blueColor()
         
     }
     
